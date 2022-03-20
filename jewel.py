ace0e769d6885845da491b27a8033905f41503a0
xb4syf <76877537+xb4syf@users.noreply.github.com>
2022-03-20 19:57:58 -0400

    Update jewel.py

--- jewel.py
+++ jewel.py
@@ -120,7 +120,6 @@ class Jewel:
                         # To handle the errors:
                         try:
                             client.send(b"HTTP/1.1 200 OK\r\n")
-                            #client.sendall("Server: xb4syf".encode())
                             client.sendall(("Server: xb4syf\r\nContent-Length: {:}\r\n\r\n".format(header)).encode())
                             client.sendall(file_context)
                         except BlockingIOError or BrokenPipeError:
